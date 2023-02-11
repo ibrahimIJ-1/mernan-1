d9592a4ac281dda9cc5804b6fa0903ae07d29ddb
ibrahimIJ-1 <104836579+ibrahimIJ-1@users.noreply.github.com>
2023-02-11 10:08:08 +0300

    bug

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,16 +8,15 @@ import { StateContext } from "../StateProvider";
 import { useTranslation } from "react-i18next";
 
 //** import images */
-import image1 from '../../assets/images/portfolio1.png';
-import image2 from '../../assets/images/portfolio2.png';
-import image3 from '../../assets/images/portfolio3.png';
+import image1 from "../../assets/images/portfolio1.png";
+import image2 from "../../assets/images/portfolio2.png";
+import image3 from "../../assets/images/portfolio3.png";
 
 //** import swiper for slider */
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
-
 
 const Portfolio = () => {
   //** this is state to change side rtl and ltr */
@@ -29,23 +28,32 @@ const Portfolio = () => {
     <div className="portfolio" dir={`${changeSide === "ar" ? "rtl" : "ltr"}`}>
       <h2>{t("portfolio")}</h2>
       <div className="portfolio_card">
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={100}
-        centeredSlides={true}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          "clickable": true}}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide><img src={image1} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image2} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image3} alt=""/></SwiperSlide>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={100}
+          centeredSlides={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+            el: null,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+          loop={true}
+        >
+          <SwiperSlide>
+            <img src={image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image3} alt="" />
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
--- src/components/PreviousClients/PreviousClients.js
+++ src/components/PreviousClients/PreviousClients.js
@@ -1,23 +1,22 @@
 import React, { useContext } from "react";
 
-import './PreviousClients.scss';
+import "./PreviousClients.scss";
 
 //** state management */
 import { StateContext } from "../StateProvider";
 
 import { useTranslation } from "react-i18next";
 
-import image from '../../assets/images/brand.png';
+import image from "../../assets/images/brand.png";
 
 //** import swiper for slider */
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 
 //** import components */
-import Card from './Card/Card';
-
+import Card from "./Card/Card";
 
 const PreviousClients = () => {
   //** this is state to change side rtl and ltr */
@@ -26,52 +25,84 @@ const PreviousClients = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="previous_clients" dir={`${changeSide === "ar" ? "rtl" : "ltr"}`}>
+    <div
+      className="previous_clients"
+      dir={`${changeSide === "ar" ? "rtl" : "ltr"}`}
+    >
       <h2>{t("previous_clients")}</h2>
       <div className="mt-5">
-      <Swiper
-        slidesPerView={7}
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
-        <SwiperSlide><img src={image} alt=""/></SwiperSlide>
+        <Swiper
+          slidesPerView={7}
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          navigation={false}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image} alt="" />
+          </SwiperSlide>
         </Swiper>
       </div>
-      
+
       <div className="cards">
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={100}
-        centeredSlides={true}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          "clickable": true}}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide><Card/></SwiperSlide>
-        <SwiperSlide><Card/></SwiperSlide>
-        <SwiperSlide><Card/></SwiperSlide>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={100}
+          centeredSlides={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+            el: null,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <Card />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card />
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
