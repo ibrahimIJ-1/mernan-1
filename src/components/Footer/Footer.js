248d576d052bbc9f3e02dd4c5a578543f3dfef52
ibrahimIJ-1 <104836579+ibrahimIJ-1@users.noreply.github.com>
2023-02-11 19:57:05 +0300

    arrows added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,14 @@
-import React , {useState , useContext , useRef} from 'react';
+import React, { useState, useContext, useRef } from "react";
 
 //** state management */
-import {StateContext} from '../StateProvider';
+import { StateContext } from "../StateProvider";
 
 //** import file scss*/
-import './Footer.scss';
-
+import "./Footer.scss";
 
 //** translation  */
-import { useTranslation } from 'react-i18next';
-import i18next from 'i18next';
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
 
 //**import icons from react-icons */
 import { BsArrowRight } from "react-icons/bs";
@@ -23,126 +22,171 @@ import { FaTwitter } from "react-icons/fa";
 import { FaBehance } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 
+const Footer = ({ color1, color2, image }) => {
+  //** this is state to change side rtl and ltr */
+  const { changeSide, setChangeSide } = useContext(StateContext);
 
-const Footer = ({color1 , color2 , image}) => {
+  const { t } = useTranslation();
 
-    //** this is state to change side rtl and ltr */
-    const { changeSide  , setChangeSide } = useContext(StateContext)
+  const ref = useRef();
 
-    const { t } = useTranslation();
-
-    const ref = useRef();
-
-     //** function to change language from english to arabic or from arabic to english */
-    const ChangeLanguage = ()=>{
-    if(ref.current.innerText === "اللغة الإنجليزية"){
-        i18next.changeLanguage('en')
-        setChangeSide("en");
-    }else{
-        i18next.changeLanguage('ar')
-        setChangeSide("ar");
+  //** function to change language from english to arabic or from arabic to english */
+  const ChangeLanguage = () => {
+    if (ref.current.innerText === "اللغة الإنجليزية") {
+      i18next.changeLanguage("en");
+      setChangeSide("en");
+    } else {
+      i18next.changeLanguage("ar");
+      setChangeSide("ar");
     }
-}
-    const styles={
-        style1:{
-            background:`${color1}`,
-            color:`${color2}`
-        },
+  };
+  const styles = {
+    style1: {
+      background: `${color1}`,
+      color: `${color2}`,
+    },
 
-        style2:{
-            color:`${color2}`
-        }
-        
-    }
+    style2: {
+      color: `${color2}`,
+    },
+  };
 
-    return (
-    <div className="footer" style={styles.style1} dir={`${changeSide === "ar" ? "rtl" : "ltr"}`}>
-        <div className="head">
-            <div className="logo">
-                <img src={image} alt=""/>
-            </div>
-            <div>
-            <button className="work_with">
-                <span>{t("work_with_us")}</span>
-                <span>
-                    {
-                        changeSide === "ar" ? <BsArrowLeft className="arrow"/> : <BsArrowRight className="arrow"/>
-                    }
-                </span>
-            </button>
-            </div>
+  return (
+    <div
+      className="footer"
+      style={styles.style1}
+      dir={`${changeSide === "ar" ? "rtl" : "ltr"}`}
+    >
+      <div className="head">
+        <div className="logo">
+          <img src={image} alt="" />
         </div>
-        <div className="body">
-            <div>
-                <h3 style={styles.style2}>{t("about_mernan")}</h3>
-                <ul style={styles.style2}>
-                    <li>
-                        <a style={styles.style2} href="@">{t("our_services")}</a>
-                    </li>
-                    <li>
-                        <a style={styles.style2} href="@">{t("what_makes_difference")}</a>
-                    </li>
-                    <li>
-                        <a style={styles.style2} href="@">{t("pricing")}</a>
-                    </li>
-                    <li>
-                        <a style={styles.style2} href="@">{t("blog")}</a>
-                    </li>
-                    <li>
-                        <a style={styles.style2} href="@">{t("frequently_asked_questions")}</a>
-                    </li>
-                    <li>
-                        <a style={styles.style2} href="@">{t("contact_us")}</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h3 style={styles.style2}>{t("join_us")}</h3>
-                <ul>
-                    <li>
-                        <a style={styles.style2} href="#">{t("join_our_team")}</a>
-                    </li>
-                    <li>
-                        <a style={styles.style2} href="#">{t("our_partners")}</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h3 style={styles.style2}>{t("more")}</h3>
-                <ul>
-                    <li>
-                        <a style={styles.style2} href="#">{t("terms_and_Conditions")}</a>
-                    </li>
-                    <li>
-                        <a style={styles.style2} href="#">{t("privacy_policy")}</a>
-                    </li>
-                    <li onClick={ChangeLanguage}>
-                        <a style={styles.style2} href="#" ref={ref}>{t("arabic_language")}</a>
-                    </li>
-                </ul>
-            </div>
+        <div>
+          <button className="work_with">
+            <span>{t("work_with_us")}</span>
+            <span>
+              {changeSide === "ar" ? (
+                <BsArrowLeft className="arrow" />
+              ) : (
+                <BsArrowRight className="arrow" />
+              )}
+            </span>
+          </button>
+        </div>
+      </div>
+      <div className="body">
+        <div>
+          <h3 style={styles.style2}>{t("about_mernan")}</h3>
+          <ul style={styles.style2}>
+            <li>
+              <a style={styles.style2} href="@">
+                {t("our_services")}
+              </a>
+            </li>
+            <li>
+              <a style={styles.style2} href="@">
+                {t("what_makes_difference")}
+              </a>
+            </li>
+            <li>
+              <a style={styles.style2} href="@">
+                {t("pricing")}
+              </a>
+            </li>
+            <li>
+              <a style={styles.style2} href="@">
+                {t("blog")}
+              </a>
+            </li>
+            <li>
+              <a style={styles.style2} href="@">
+                {t("frequently_asked_questions")}
+              </a>
+            </li>
+            <li>
+              <a style={styles.style2} href="@">
+                {t("contact_us")}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 style={styles.style2}>{t("join_us")}</h3>
+          <ul>
+            <li>
+              <a style={styles.style2} href="#">
+                {t("join_our_team")}
+              </a>
+            </li>
+            <li>
+              <a style={styles.style2} href="#">
+                {t("our_partners")}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 style={styles.style2}>{t("more")}</h3>
+          <ul>
+            <li>
+              <a style={styles.style2} href="#">
+                {t("terms_and_Conditions")}
+              </a>
+            </li>
+            <li>
+              <a style={styles.style2} href="#">
+                {t("privacy_policy")}
+              </a>
+            </li>
+            <li onClick={ChangeLanguage}>
+              <a style={styles.style2} href="#" ref={ref}>
+                {t("arabic_language")}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="social">
+        <div className="media" style={styles.style2}>
+          <a style={styles.style2} href="https://linkedin.com">
+            <FaLinkedin />
+          </a>
+          <a
+            style={styles.style2}
+            href="https://facebook.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaFacebookF />
+          </a>
+          <a style={styles.style2} href="https://snapchat.com">
+            <FaSnapchatGhost />
+          </a>
+          <a style={styles.style2} href="https://tiktok.com">
+            <FaTiktok />
+          </a>
+          <a style={styles.style2} href="https://youtube.com">
+            <FaYoutube />
+          </a>
+          <a style={styles.style2} href="https://twitter.com">
+            <FaTwitter />
+          </a>
+          <a style={styles.style2} href="https://instegram.com">
+            <AiFillInstagram />
+          </a>
+          <a style={styles.style2} href="https://behance.com">
+            <FaBehance />
+          </a>
         </div>
 
-        <div className="social">
-
-            <div className="media" style={styles.style2}>
-                <a style={styles.style2} href="https://linkedin.com"><FaLinkedin/></a>
-                <a style={styles.style2} href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF/></a>
-                <a style={styles.style2} href="https://snapchat.com"><FaSnapchatGhost/></a>
-                <a style={styles.style2} href="https://tiktok.com"><FaTiktok/></a>
-                <a style={styles.style2} href="https://youtube.com"><FaYoutube/></a>
-                <a style={styles.style2} href="https://twitter.com"><FaTwitter/></a>
-                <a style={styles.style2} href="https://instegram.com"><AiFillInstagram/></a>
-                <a style={styles.style2} href="https://behance.com"><FaBehance/></a>
-            </div>
-
-            <div className="copy_right">
-                <span>{t("copy_right")}</span>
-            </div>
-            <div></div>
+        <div className="copy_right">
+          <span>{t("copy_right")}</span>
         </div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/WhoMernan/WhoMernan.js
+++ src/pages/WhoMernan/WhoMernan.js
@@ -1,63 +1,125 @@
-import React from 'react'
-
+// import React, { useEffect, useRef } from "react";
+// import ReactDOM from "react-dom";
+// import $ from "jquery";
+// import "jquery-connections";
+import React, { useState } from "react";
 //** import components */
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
-import Welcome from '../../components/Welcome/Welcome';
-import WhatServices from '../../components/WhatServices/WhatServices';
-import WhatMakesMernan from '../../components/WhatMakesMernan/WhatMakesMernan';
-import JoinUs from '../../components/JoinUs/JoinUs';
-import AboutMernan from '../../components/AboutMernan/AboutMernan';
-import OurValues from '../../components/OurValues/OurValues';
-import OurPartners from '../../components/OurPartners/OurPartners';
-import PreviousClients from '../../components/PreviousClients/PreviousClients';
-import OurLocations from '../../components/OurLocations/OurLocations';
-import RequestFreeConsulting from '../../components/RequestFreeConsulting/RequestFreeConsulting';
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Welcome from "../../components/Welcome/Welcome";
+import WhatServices from "../../components/WhatServices/WhatServices";
+import WhatMakesMernan from "../../components/WhatMakesMernan/WhatMakesMernan";
+import JoinUs from "../../components/JoinUs/JoinUs";
+import AboutMernan from "../../components/AboutMernan/AboutMernan";
+import OurValues from "../../components/OurValues/OurValues";
+import OurPartners from "../../components/OurPartners/OurPartners";
+import PreviousClients from "../../components/PreviousClients/PreviousClients";
+import OurLocations from "../../components/OurLocations/OurLocations";
+import RequestFreeConsulting from "../../components/RequestFreeConsulting/RequestFreeConsulting";
+
+import "./Arrows/arrow.css";
 
 //** import logo */
-import logo from '../../assets/images/mernan_white.png';
+import logo from "../../assets/images/mernan_white.png";
+import rightArrow from "../../assets/images/arrow-vector-right.svg";
+import leftArrow from "../../assets/images/arrow-vector-left.svg";
+// function Arrow({ start, end }) {
+//   const [arrowStart, setArrowStart] = useState(start);
+//   const [arrowEnd, setArrowEnd] = useState(end);
 
-const WhoMernan = () => {
-
+//   return (
+//     <div className="container">
+//       <div
+//         className="arrow start"
+//         style={{ top: arrowStart.y, left: arrowStart.x }}
+//       ></div>
+//       <div
+//         className="arrow end"
+//         style={{ top: arrowEnd.y, left: arrowEnd.x }}
+//       ></div>
+//     </div>
+//   );
+// }
+function ArrowLeft({ start, classes }) {
+  const [arrowStart, setArrowStart] = useState(start);
+  const [arrowClass, setArrowClass] = useState(classes);
   return (
-    <div> 
-        {/* Header */}
-        <Header/>
-
-        {/* Welcome */}
-        <Welcome/>
-
-        {/* What Services */}
-        <WhatServices/>
-
-        {/* What make mernan */}
-        <WhatMakesMernan/>
-
-        {/* Join Us */}
-        <JoinUs/>
-
-        {/* About mernan */}
-        <AboutMernan/>
-
-        {/* Our Values */}
-        <OurValues/>
-
-        {/* Our Partners */}
-        <OurPartners color1={"#596773"} color2={"#FDFDFE"}/>
-
-        {/*Previous Clients*/}
-        <PreviousClients/>
-
-        {/*Our Locations*/}
-        <OurLocations/>
-
-        <RequestFreeConsulting/>
-        
-        {/* Footer */}
-        <Footer color1={"#39858E"} color2={"#FDFDFE"} image={logo}/>
+    <div className="container">
+      <div
+        className={arrowClass}
+        style={{ top: arrowStart.y, left: arrowStart.x }}
+      >
+        <img src={leftArrow} alt="" />
+      </div>
     </div>
-
-  )
+  );
 }
+function ArrowRight({ start, classes }) {
+  const [arrowStart, setArrowStart] = useState(start);
+  const [arrowClass, setArrowClass] = useState(classes);
+  return (
+    <div className="container">
+      <div
+        className={arrowClass}
+        style={{ top: arrowStart.y, left: arrowStart.x }}
+      >
+        <img src={rightArrow} alt="" />
+      </div>
+    </div>
+  );
+}
+const WhoMernan = () => {
+  const [start, setStart] = useState({ x: "5%", y: "350px" });
+  const [start1, setStart1] = useState({ x: "5%", y: "600px" });
+  const [classes, setClasses] = useState("arrow start");
+  // const chartContainer = useRef(null);
 
-export default WhoMernan
+  // useEffect(() => {
+  //   const $chartContainer = $(chartContainer.current);
+  //   $chartContainer.connections({
+  //     to: ".second",
+  //   });
+  // }, []);
+  return (
+    <div>
+      {/* Header */}
+      <Header />
+      {/* Welcome */}
+      <ArrowLeft start={start} classes={classes} />
+      <Welcome />
+
+      {/* What Services */}
+      <ArrowRight start={start1} classes={classes} />
+      <WhatServices />
+
+      {/* What make mernan */}
+      <ArrowLeft start={start1} classes={classes} />
+      <WhatMakesMernan />
+
+      {/* Join Us */}
+      <JoinUs />
+
+      {/* About mernan */}
+      <AboutMernan />
+
+      {/* Our Values */}
+      <OurValues />
+
+      {/* Our Partners */}
+      <OurPartners color1={"#596773"} color2={"#FDFDFE"} />
+
+      {/*Previous Clients*/}
+      <PreviousClients />
+
+      {/*Our Locations*/}
+      <OurLocations />
+
+      <RequestFreeConsulting />
+
+      {/* Footer */}
+      <Footer color1={"#39858E"} color2={"#FDFDFE"} image={logo} />
+    </div>
+  );
+};
+
+export default WhoMernan;
